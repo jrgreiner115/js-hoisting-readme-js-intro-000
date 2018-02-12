2d9acf388f68952da9ac9062387490f169fe6273
Joe Greiner <jrgreiner115@gmail.com>
2018-02-12 16:17:42 +0000

    automatically backed up by learn

--- hoisting.js
+++ hoisting.js
@@ -1,7 +1,8 @@
-var lyric = "maybe";
+
  var thisIsCrazy = function (){
     console.log("hey!!!");
 function callMe() {
+  var lyric = "maybe";
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
